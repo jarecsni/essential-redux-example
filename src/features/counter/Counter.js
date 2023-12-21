c7028ba48aca615dd64cb88c49a6aab471301393
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-12-21 11:02:36 +0000

    Setting up architecture, counter slice

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { increment } from './CounterSlice';
+
+export function Counter() {
+    return (
+        <div>
+            <button
+                onClick={()=>{}}
+            >
+            +
+            </button>
+        </div>
+    );
+}
