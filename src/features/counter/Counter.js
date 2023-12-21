4beb03139bfb1cf56a32610166902b6283785e2b
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-12-21 11:34:32 +0000

    incrementByAmount

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { increment, decrement, selectCount } from './CounterSlice';
+import { increment, decrement, incrementByAmount, selectCount } from './CounterSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
 export function Counter() {
     const count = useSelector(selectCount);
     const dispatch = useDispatch();
+    const [incrementBy, setIncrementBy] = useState('2');
+    const incrementByValue = Number(incrementBy) || 0;
     return (
         <div>
             <button
@@ -12,12 +14,22 @@ export function Counter() {
             >
                 +
             </button>
+            <div style={{ display: 'inline-block', padding: '10px' }}>{count}</div>
             <button
                 onClick={() => { dispatch(decrement()) }}
             >
                 -
             </button>
-            {count}
+            <input
+                aria-label='Set Increment Amount'
+                value={incrementBy}
+                onChange={(e) => setIncrementBy(e.target.value)}
+            ></input>
+            <button
+                onClick={()=>dispatch(incrementByAmount(incrementByValue))}
+            >
+                Add amount
+            </button>
         </div>
     );
 }
